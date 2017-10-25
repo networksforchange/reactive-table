2c32c037a2572c7c090d6f20a2c5d6d43af55427
Wayne Uong <wayne@networksforchange.com>
2017-10-25 11:12:10 -0700

    'bump ver'

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.8.44",
+  version: "0.8.47",
   name: "networksforchange:reactive-table",
   git: "https://github.com/networksforchange/reactive-table.git"
 });
